a9244e792d34a7ac3e15a46582615d71f8a6a0f3
miloud26 <fiverr.miloud@gmail.com>
2026-04-08 22:42:23 +0100

    all store

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { Routes, Route, useNavigate } from "react-router-dom";
+import React, { lazy, useEffect, startTransition, Suspense } from "react";
+
+const Page01 = lazy(() => import("./Page01.jsx"));
+
+const Error = lazy(() => import("./components/Error"));
+
+const App = () => {
+  const navigate = useNavigate();
+  const url = localStorage.getItem("baseURL");
+
+  useEffect(() => {
+    if (
+      url &&
+      (window.location.pathname === "/" ||
+        window.location.pathname === "/products" ||
+        window.location.pathname === "/products/")
+    ) {
+      startTransition(() => {
+        navigate(url);
+      });
+    }
+  }, [url, navigate]);
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="*" element={<Error />} />
+
+        <Route path="/products/page01" element={<Page01 />} />
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    id: "l-2",
+    name: "Robe de Soirée ✨",
+    slug: "",
+    phone: "",
+    price: "13900",
+    delevery: "500",
+    clr: ["Noir"],
+    size: [40, 42, 44, 46, 48],
+    descImag: "https://i.ibb.co/VW63C0QS/3.webp",
+    themImg: "https://i.ibb.co/TD5w28zh/electroneque.webp",
+  },
+];
