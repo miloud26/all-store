be2dfccc129675650380d26753e77b09de8558b6
miloud26 <fiverr.miloud@gmail.com>
2026-05-01 22:55:15 +0100

    new product

--- src/data.js
+++ src/data.js
@@ -57,7 +57,7 @@ export const data = [
     name: "Robe Hijab ✨",
     slug: "",
     phone: "",
-    price: "4300",
+    price: "3900",
     delevery: "600",
     clr: ["Bleu Nuit", "Orange", "Marron"],
     size: [40, 42, 44, 46, 48],
